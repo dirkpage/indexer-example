83866159c9e305b2dde851f105e5080ad64871d2
Dirk Page <dpage@eco.com>
2025-02-13 19:14:21 -0600

    Added intent created log, removing unecessary attributes

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -2,10 +2,6 @@ import { index, onchainTable } from "ponder";
 
 function getSharedAttributes(t: any) {
   return {
-    version: t.text().notNull(),
-
-    from: t.hex().notNull(),
-    to: t.hex(),
     transactionHash: t.hex().notNull(),
     transactionIndex: t.integer().notNull(),
     evt_log_index: t.integer().notNull(),
@@ -22,8 +18,6 @@ export const intent = onchainTable(
   (t) => ({
     hash: t.hex().primaryKey(),
     creator: t.hex().notNull(),
-
-    params: t.json().notNull(),
 
     ...getSharedAttributes(t),
   }),
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,22 @@ import { ponder } from "ponder:registry";
 import {
   intent,
 } from "ponder:schema";
-import { replaceBigInts } from "ponder";
 
 ponder.on("IntentSource:IntentCreated", async ({ event, context }) => {
   const args = event.args as Record<string, any>;
   const hash = args._hash || args.hash;
 
-  const creator = args._creator || args.creator;
+  if (hash) {
+    console.log(`Intent created: ${hash}`);
+  }
 
-  const params = replaceBigInts(args, (v) => v.toString());
+  const creator = args._creator || args.creator;
 
   await context.db
     .insert(intent)
     .values({
       hash,
       creator,
-      params,
-      version: "0",
-      from: event.transaction.from,
-      to: event.transaction.to,
       evt_log_index: event.log.logIndex,
       evt_log_address: event.log.address,
       transactionHash: event.transaction.hash,
